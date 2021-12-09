58e245f70d201b80aab92bc49f4659878601c586
procodingclass <88050205+procodingclass@users.noreply.github.com>
2021-12-09 17:03:24 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -5,23 +5,24 @@ const Body = Matter.Body;
 
 
 var engine, world;
-var box1, box2, box3,box4,box5,box6,box7,box8,box9,box10,box11,box12,box13,box14,box15,box16,box17,box18,box19,box20, ground;
-var ball, rope;
+var box1, box2, box3,ground;
+var ball;
 
 function setup() {
-  createCanvas(600, 400);
+  createCanvas(850, 400);
   engine = Engine.create();
   world = engine.world;
 
-  ground = new Box(0, 380, 600, 20);
-  
-  
-  box1 = new Box(300, 100, 70, 70);
-  box2 = new Box(400, 100, 70, 70);
-  box3 = new Box(500, 100, 70, 70);
-  
-  ball = new Ball(100, 100, 40, 40);
-  
+
+  ball = new Ball(50, 100, 30);
+8
+  ground = new Box(0, 380,850, 20);
+
+  Body.setStatic(ground.body, true);
+
+  box1 = new Box(100, 100, 70, 70);
+  box2 = new Box(200, 100, 70, 70);
+  box3 = new Box(300, 100, 70, 70);
 
 }
 
@@ -32,11 +33,6 @@ function draw() {
   box1.display();
   box2.display();
   box3.display();
-  
 
   ball.display();
 }
-
-
-
-
